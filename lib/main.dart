315e5a0cde78613d6a3ad7aab555ef31ebd27203
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-10-30 23:30:44 +0200

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main (   ) { }
+class Ecommerce extends StatelessWidget {
+  const Ecommerce({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
