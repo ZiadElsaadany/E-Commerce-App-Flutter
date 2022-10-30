79685c317b4859a97609f4ff04acf694374ff29d
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-10-31 00:34:53 +0200

    custom button added

---
+++ lib/view/auth/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../utls/app_constant.dart';
+
+class CustomButton extends StatelessWidget {
+   CustomButton({Key? key,required this.word}) : super(key: key);
+String word;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ButtonStyle(
+            padding: MaterialStateProperty.all(EdgeInsets.all(20)),
+            backgroundColor: MaterialStateProperty.all(ConstantApp.greenColor),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
+            ))
+        ),
+        onPressed: (){}, child: Text(
+      '$word',
+      style: TextStyle(
+          fontSize: 18
+      ),
+    ));
+  }
+}
