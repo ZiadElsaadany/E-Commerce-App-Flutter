e093fcb9ba9dffa2fd89159d6840f55351cca644
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-10-31 00:00:23 +0200

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
+import 'package:e_commerce_intern/view/auth/login_screen/login_screen.dart';
+import 'package:e_commerce_intern/view/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main (   ) { }
+void main (   ) {
+  runApp(Ecommerce());
+}
 class Ecommerce extends StatelessWidget {
   const Ecommerce({Key? key}) : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id:(ctx)=> SplashScreen(),
+        LoginScreen.id :(ctx)=>LoginScreen()
+      },
+    );
   }
 }
---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:e_commerce_intern/view/auth/login_screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String id=  'Splash' ;
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late Timer timer ;
+
+  goNext( )=> Navigator.pushReplacementNamed(context,LoginScreen.id );
+
+  startDelay(){
+    timer=Timer(const Duration(
+        milliseconds: 3000000
+    ), ()=> goNext()
+    );
+  }
+  @override
+  void initState() {
+    super.initState();
+    startDelay()  ;
+  }
+  @override
+  void dispose() {
+    timer.cancel() ;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+        width: double.infinity,
+        child: Column (
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SizedBox(
+                width: MediaQuery.of(context).size.width*0.8,
+                child: Image.asset('assets/images/img.png')),
+          ) ,
+          SizedBox(
+              width: MediaQuery.of(context).size.width*0.4,
+              child: Image.asset('assets/images/img_1.png')) ,
+          SizedBox(
+            height: MediaQuery.of(context).size.height*0.02,
+          ),
+        ],
+      ),
+    )) ;
+  }
+}
